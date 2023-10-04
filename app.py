8ef8b228823e0619b39abbed7b936850be5cd5d4
masood <masood@ubuntu.myguest.virtualbox.org>
2023-10-04 21:14:52 +0300

    login page design added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='styles')
 app.secret_key = 'your_secret_key'  # Replace with a secure secret key
 
 # Replace with a database for storing user credentials
