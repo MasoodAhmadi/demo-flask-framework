2801f52e53f09d6ceab4b26f8b1db2ac2a7d343c
masood <masood@ubuntu.myguest.virtualbox.org>
2023-10-04 17:43:03 +0300

    login page added

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('base.html')
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
